745a6e50ec6a7f2f9ed9e9adf6bc27ecbad734c2
sagipramod <pramod.sagi@gmail.com>
2018-01-24 16:39:01 +0530

    Add files via upload

---
+++ Award Bonus/LoyaltyAction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Suncor.OPE.Data;
+using Suncor.Framework.Data;
+using Suncor.OPE.Entities;
+
+namespace Suncor.OPE.BusinessService.LoyaltyAction
+{
+    public abstract class LoyaltyAction
+    {
+        public string LoyaltyCardNumber { get; set; }
+        public int ProductId { get; set; }
+        public string LinkedCardNumber { get; set; }
+        public string BonusType { get; set; }
+        public string CardType { get; set; }
+
+
+        public LoyaltyAction(string loyaltyCardNumber, int productId,string linkedCardNumber, string bonusType, string cardType)
+        {
+            this.LoyaltyCardNumber = loyaltyCardNumber;
+            this.ProductId = productId;
+            this.LinkedCardNumber = linkedCardNumber;
+            this.BonusType = bonusType;
+            this.CardType = cardType;
+        }
+        
+        public LoyaltyAction(string loyaltyCardNumber, int productId, string bonusType, string linkedCardNumber)
+        {
+            LoyaltyCardNumber = loyaltyCardNumber;
+            ProductId = productId;
+            BonusType = bonusType;
+            LinkedCardNumber = linkedCardNumber;
+        }
+        public virtual bool QualifyForBonus()
+        {
+            return true;
+        }
+        public virtual bool UpdateBonusGivenDate()
+        {
+            return true;
+        }
+    }
+}
---
+++ Award Bonus/ReloadCard.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Suncor.OPE.BusinessService.LoyaltyAction
+{
+    public class ReloadCard : LoyaltyAction
+    {
+        public ReloadCard (string loyaltyCardNumber, int productId, string bonusType, string linkedCardNumber) : base (loyaltyCardNumber, productId, bonusType, linkedCardNumber)
+        {
+        }
+
+
+    }
+}
